480203e079216fcd29e6905006c54e37b6819aec
Rohan Dey <rohandey@LPMB-KOL-Rohan.local>
2019-03-05 11:51:00 +0530

    First commit

---
+++ out/extension.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider('*', {
+        provideCompletionItems(document, position, token) {
+            return [new vscode.CompletionItem('Hello')];
+        }
+    }));
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
