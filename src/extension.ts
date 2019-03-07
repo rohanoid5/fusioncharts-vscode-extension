a8568d2b6db8926d05a8e64e3ec8b5ea677a32fd
Rohan Dey <rohanoid5@gmail.com>
2019-03-07 18:55:24 +0530

    Added all chart types and its attrs

--- src/extension.ts
+++ src/extension.ts
@@ -5,17 +5,17 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(
-    vscode.languages.registerCompletionItemProvider('*', {
-      provideCompletionItems(
-        document: vscode.TextDocument,
-        position: vscode.Position,
-        token: vscode.CancellationToken
-      ) {
-        return [new vscode.CompletionItem('Hello')];
-      }
-    })
-  );
+  // context.subscriptions.push(
+  //   vscode.languages.registerCompletionItemProvider('*', {
+  //     provideCompletionItems(
+  //       document: vscode.TextDocument,
+  //       position: vscode.Position,
+  //       token: vscode.CancellationToken
+  //     ) {
+  //       return [new vscode.CompletionItem('Hello')];
+  //     }
+  //   })
+  // );
 }
 
 // this method is called when your extension is deactivated
